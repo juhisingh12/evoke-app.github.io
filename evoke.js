9a32dfa30402d1d8177b62666c3c7d55e9f1441d
juhisingh12 <juhi2721@gmail.com>
2023-12-19 21:42:39 +0530

    evoke-app

---
+++ evoke.js
@@ -0,0 +1,224 @@
+let Service=document.getElementById("Service")
+console.log(Service);
+let juhi=document.getElementById("sec")
+console.log(sec);
+
+
+Service.addEventListener("mouseover",()=>{
+    let service=document.getElementById("article-1")
+    console.log(service);
+    service.style.display="block"
+
+
+    sec.addEventListener("mouseover",()=>{
+        service.style.display="none"
+
+    })
+      let ind1=document.getElementById("aside-1")
+    console.log(ind1);
+    ind1.style.visibility="hidden"
+
+    let partner1=document.getElementById("aside3")
+    console.log(partner1);
+    partner1.style.visibility="hidden"
+
+    let ins1=document.getElementById("aside2")
+    console.log(ins1);
+    ins1.style.display="none"
+
+    let about1=document.getElementById("aside5")
+    console.log(about1);
+    about1.style.display="none"
+
+    let solution1=document.getElementById("aside4")
+    console.log(solution1);
+    solution1.style.display="none"
+  
+})
+
+//....................................................................................
+
+let ind=document.getElementById("ind")
+console.log(ind);
+ind.addEventListener("mouseover",()=>{
+    let ind1=document.getElementById("aside-1")
+    console.log(ind1);
+    ind1.style.visibility="visible"
+
+
+    sec.addEventListener("mouseover",()=>{
+        ind1.style.display="none"
+
+    })
+    let service=document.getElementById("article-1")
+    console.log(service);
+    service.style.display="none"
+
+    let partner1=document.getElementById("aside3")
+    console.log(partner1);
+    partner1.style.visibility="hidden"
+
+
+    let ins1=document.getElementById("aside2")
+    console.log(ins1);
+    ins1.style.display="none"
+
+    let about1=document.getElementById("aside5")
+    console.log(about1);
+    about1.style.display="none"
+
+     let solution1=document.getElementById("aside4")
+    console.log(solution1);
+    solution1.style.display="none"
+
+
+     
+})
+
+//............................................................................................
+
+let ins=document.getElementById("ins")
+console.log(ins);
+ins.addEventListener("mouseover",()=>{
+    let ins1=document.getElementById("aside2")
+    console.log(ins1);
+    ins1.style.display="block"
+
+    sec.addEventListener("mouseover",()=>{
+        ins1.style.display="none"
+    })
+    let service=document.getElementById("article-1")
+    console.log(service);
+    service.style.display="none"
+
+    let ind1=document.getElementById("aside-1")
+    console.log(ind1);
+    ind1.style.visibility="hidden"
+
+    let partner1=document.getElementById("aside3")
+    console.log(partner1);
+    partner1.style.visibility="hidden"
+
+     let about1=document.getElementById("aside5")
+    console.log(about1);
+    about1.style.display="none"
+
+      let solution1=document.getElementById("aside4")
+    console.log(solution1);
+    solution1.style.display="none"
+
+
+
+})
+
+//..............................................................................................
+
+let partner=document.getElementById("partner")
+console.log(partner);
+partner.addEventListener("mouseover",()=>{
+    let partner1=document.getElementById("aside3")
+    console.log(partner1);
+    partner1.style.visibility="visible"
+
+    sec.addEventListener("mouseover",()=>{
+        partner1.style.display="none"
+    })
+    let service=document.getElementById("article-1")
+    console.log(service);
+    service.style.display="none"
+
+    let ind1=document.getElementById("aside-1")
+    console.log(ind1);
+    ind1.style.visibility="hidden"
+
+     let ins1=document.getElementById("aside2")
+    console.log(ins1);
+    ins1.style.display="none"
+
+     let solution1=document.getElementById("aside4")
+    console.log(solution1);
+    solution1.style.display="none"
+
+     let about1=document.getElementById("aside5")
+    console.log(about1);
+    about1.style.display="none"
+
+
+})
+
+
+//.............................................................................
+
+let solution=document.getElementById("solution")
+console.log(solution);
+solution.addEventListener("mouseover",()=>{
+    let solution1=document.getElementById("aside4")
+    console.log(solution1);
+    solution1.style.display="block"
+
+    sec.addEventListener("mouseover",()=>{
+        solution1.style.display="none"
+    })
+    let service=document.getElementById("article-1")
+    console.log(service);
+    service.style.display="none"
+
+    let ind1=document.getElementById("aside-1")
+    console.log(ind1);
+    ind1.style.visibility="hidden"
+
+     let ins1=document.getElementById("aside2")
+    console.log(ins1);
+    ins1.style.display="none"
+
+     let about1=document.getElementById("aside5")
+    console.log(about1);
+    about1.style.display="none"
+
+    let partner1=document.getElementById("aside3")
+    console.log(partner1);
+    partner1.style.visibility="none"
+
+
+
+})
+
+//......................................................................................
+
+let about=document.getElementById("about")
+console.log(about);
+about.addEventListener("mouseover",()=>{
+    let about1=document.getElementById("aside5")
+    console.log(about1);
+    about1.style.display="block"
+
+    sec.addEventListener("mouseover",()=>{
+        about1.style.display="none"
+    })
+    let service=document.getElementById("article-1")
+    console.log(service);
+    service.style.display="none"
+
+    let ind1=document.getElementById("aside-1")
+    console.log(ind1);
+    ind1.style.visibility="hidden"
+
+     let ins1=document.getElementById("aside2")
+    console.log(ins1);
+    ins1.style.display="none"
+
+     let solution1=document.getElementById("aside4")
+    console.log(solution1);
+    solution1.style.display="none"
+
+    let partner1=document.getElementById("aside3")
+    console.log(partner1);
+    partner1.style.visibility="hidden"
+
+
+
+})
+
+
+
+
